436362947f5ff23fca17d8a336e4b9a836953c7d
STFleming <sf306@ic.ac.uk>
2020-12-10 21:01:48 +0000

    Starting to add the fwft fifo visulisation

---
+++ webserver/hdlvis/fifo_fwft.js
@@ -0,0 +1,24 @@
+// fifo_fwft visualisation class
+//
+// author: stf
+
+class fifo_fwft {
+
+    constructor(svg, colour, x, y, name, depth) {
+        this.svg = svg;
+        this.colour = colour;
+        this.x = x;
+        this.y = y;
+        this.name = name;
+        this.depth = depth;
+
+        this.draw();
+    }
+
+
+    draw() {
+         
+
+    }
+
+}
